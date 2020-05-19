ec3c48befde5e969974e4bea04f189dbcf51c05e
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-05-19 21:19:56 +0900

    버튼 실습

--- FirstFormApplication/Form1.cs
+++ FirstFormApplication/Form1.cs
@@ -15,6 +15,20 @@ namespace FirstFormApplication
         public Form1()
         {
             InitializeComponent();
+            myButton.Enabled = true;
+            myButton.Text = "코드에서 변경";
+            myButton.Width = 200;
+
+            for(int i = 0; i < 5; i++)
+            {
+                Button button = new Button();
+                Controls.Add(button);
+                button.Location = new Point(113, 13 + (23 + 3)*i);
+                button.Text = "동적 생성"+i+"번째";
+                button.Width = 100;
+            }
+
+
         }
     }
 }
